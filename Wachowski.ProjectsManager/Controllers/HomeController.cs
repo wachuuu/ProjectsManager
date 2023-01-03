90e556b55a53e0ffbb384e7e6783f4091ebe696c
Gabriel Wachowski <wachowski.gabriel@gmail.com>
2023-01-03 10:46:38 +0100

    delete privacy

--- Wachowski.ProjectsManager/Controllers/HomeController.cs
+++ Wachowski.ProjectsManager/Controllers/HomeController.cs
@@ -18,10 +18,6 @@ namespace Wachowski.ProjectsManager.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
